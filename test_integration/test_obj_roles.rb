da858e33b96a8a3856ac071ccc49454b844ccaf7
Nathan Smith <nate@branchout.com>
2014-02-09 14:59:30 -0800

    added roles integration test

---
+++ test_integration/test_obj_roles.rb
@@ -0,0 +1,20 @@
+require 'rscalr'
+require 'test/unit'
+
+class TestObjRoles < Test::Unit::TestCase
+  
+  def setup      
+    @dashboard = {
+      :key_id => ENV['SCALR_TEST_API_KEY'], 
+      :key_secret => ENV['SCALR_TEST_API_SECRET'], 
+      :env_id => ENV['SCALR_TEST_ENV_ID'] 
+    }    
+  end
+  
+  def test_get_role_by_name
+    role = @dashboard.get_role(ENV['SCALR_TEST_ROLE_NAME'])
+    
+    assert_not_nil(role, "Test role not found by name")
+  end
+  
+end
